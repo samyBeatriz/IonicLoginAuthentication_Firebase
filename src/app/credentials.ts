57cd597bb23e524f0b0c2cd27f00204e1e91410a
samyBeatriz <45011774+samyBeatriz@users.noreply.github.com>
2019-11-05 09:49:36 -0400

    Add files via upload

---
+++ src/app/credentials.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyC_Lji5hhXmtgDn1T16As9hqbkeY35mNTM",
+    authDomain: "manager-133d1.firebaseapp.com",
+    databaseURL: "https://manager-133d1.firebaseio.com",
+    projectId: "manager-133d1",
+    storageBucket: "manager-133d1.appspot.com",
+    messagingSenderId: "245293042042",
+    appId: "1:245293042042:web:10296bd084fc8fd5bd2ec3"
+
+};
